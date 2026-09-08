3aa34e94e3643423256373b7ce047b175d1dd9dc
신해성 <s2h0219@sogang.ac.kr>
2026-09-08 21:59:24 +0900

    feat: filter_over 함수 작성

--- solution.py
+++ solution.py
@@ -23,3 +23,15 @@ def format_receipt(name, price):
 print("\n2) format_receipt 테스트:")
 print(format_receipt("라떼", 5500))
 print(format_receipt("아메리카노", 4000))
+
+def filter_over(numbers,  threshold):
+	result = []
+	for num in numbers:
+		if num > threshold:
+			result.append(num)
+
+#최소 2가지 입력 테스트
+print("\n3 filter_over 테스트:")
+print(filter_over([10,25,3,40],20)) # [25,40]
+print(filter_over([1,5,8,12,15],10)) # [12,15]
+
