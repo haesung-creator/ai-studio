1262a9f972d88ba76f5179802671c206ab4885cf
신해성 <s2h0219@sogang.ac.kr>
2026-09-08 22:10:45 +0900

    fix 코드 수정 완료

--- solution.py
+++ solution.py
@@ -12,23 +12,27 @@ def calculator(a, b, op):
 	return "Error"
 
 
-# 최소 2가지 입력 테스트
+#최소 2가지 입력 테스트
 print("1) calculator 테스트:")
-print(calculator(10, 3, "/"))
-print(calculator(5, 0, "/"))
+print(calculator(10, 3, "/")) #3.333...
+print(calculator(5, 0, "/")) #Error
 
 def format_receipt(name, price):
-	return f"[{name}]}]" 가격: {price:,}원"
+	return f"[{name}] 가격: {price:,}원"
 
+#최소 2가지 입력 테스트
 print("\n2) format_receipt 테스트:")
-print(format_receipt("라떼", 5500))
-print(format_receipt("아메리카노", 4000))
+print(format_receipt("라떼", 5500)) # [라떼] 가격: 5,500원
+print(format_receipt("아메리카노", 4000)) # [아메리카노] 가격: 4,000원
+
 
 def filter_over(numbers,  threshold):
 	result = []
 	for num in numbers:
 		if num > threshold:
 			result.append(num)
+	return result
+	return result
 
 #최소 2가지 입력 테스트
 print("\n3 filter_over 테스트:")
